df02448072690e9e571e3b8fb7c1593bdce1ace4
Suryansh <suryanshgrover1999@gmail.com>
2022-11-28 18:54:02 +0530

    configured custom Exception for this project

---
+++ src/exception.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def error_message_detail(error_message, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = f"Error in {file_name} at line {exc_tb.tb_lineno}: {str(error_message)}."
+    return error_message
+
+
+class SensorException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        self.error_message = error_message_detail(
+            error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
