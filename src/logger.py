ac147aa1a5bc7ce79979487370868533054f0267
Suryansh <suryanshgrover1999@gmail.com>
2022-11-28 15:13:00 +0530

    configured the process of recording logs

---
+++ src/logger.py
@@ -0,0 +1,23 @@
+import logging as lg
+import os
+from datetime import datetime
+
+
+# Name of the file where the logs are to be recorded
+LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
+
+# Logs directory
+LOG_FILE_DIR = os.path.join(os.getcwd(), ".logs")
+
+# Create the dir if not available
+os.makedirs(LOG_FILE_DIR, exist_ok=True)
+
+# Log file path
+LOG_FILE_PATH = os.path.join(LOG_FILE_DIR, LOG_FILE_NAME)
+
+# Logs' configuration
+lg.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s %(message)s",
+    level=lg.INFO
+)
