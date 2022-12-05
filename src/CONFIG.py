781c17c77b18bd0b6c37e7305dce77168df2632e
Suryansh <suryanshgrover1999@gmail.com>
2022-12-05 09:33:16 +0530

    separated the read_params method from the class

--- src/CONFIG.py
+++ src/CONFIG.py
@@ -7,32 +7,28 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    """Helps to access the secret and protected params that cannot be exposed beyond the scope of the project.
-    Moreover, provides us with a method to read the global params from the provided path of the configuration
-    file.
+    """Helps to access the secret and protected params declared inside the .env that cannot be exposed beyond 
+    the scope of the project.
     """
     load_dotenv()
-    mongodb_url:str = os.getenv("MONGO_DB_URL")
-    aws_access_key_id:str = os.getenv("AWS_ACCESS_KEY_ID")
-    aws_secret_access_key:str = os.getenv("AWS_SECERET_ACCESS_KEY")
-
-    def read_params(self, config_file_path):
-        """Read and return the dictionary containing all the paths and params from the parameterized
-        configuration file's path.
-
-        Args:
-            file_path (string): Path of the configuration file.
-
-        Returns:
-            dict: Contains all the params and paths in the form of key:val pair.
-        """
-        try:
-            with open(config_file_path) as yaml_file:
-                config = yaml.safe_load(yaml_file)
-            return config
-        except Exception as e:
-            lg.exception(e)
+    mongodb_url: str = os.getenv("MONGO_DB_URL")
+    aws_access_key_id: str = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_secret_access_key: str = os.getenv("AWS_SECERET_ACCESS_KEY")
 
 
-if __name__ == "__main__":
-    Config()
+def read_params(config_file_path):
+    """Retrieve all the params or paths declared inside the desired config file and returns the dictionary
+    containing all of them.
+
+    Args:
+        file_path (string): Path of the configuration file.
+
+    Returns:
+        dict: Contains all the params and paths in the form of key:val pair.
+    """
+    try:
+        with open(config_file_path) as yaml_file:
+            config = yaml.safe_load(yaml_file)
+        return config
+    except Exception as e:
+        lg.exception(e)
